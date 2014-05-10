eedfcfd039553f226b537e354b25374f693be295
Lukasz Paczynski <luki00765@gmail.com>
2014-05-10 14:24:23 +0200

    add ViewModels and create Image link

---
+++ SimpleTeamPlayers/ViewModels/ImageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SimpleTeamPlayers.Models;
+
+namespace SimpleTeamPlayers.ViewModels
+{
+    public class ImageViewModel
+    {
+        public Image image = new Image();
+        public List<Image> list_image = new List<Image>();
+    }
+}
